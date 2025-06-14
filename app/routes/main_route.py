4d71da9d504618f7a1d8e1c99ce3e5d4ec4f5b52
Haoran Tang <h.tang-6@student.tudelft.nl>
2025-06-14 03:58:36 +0200

    fix: bug

--- app/routes/main_route.py
+++ app/routes/main_route.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, render_template, current_app
 from lib_version.version_awareness import VersionUtil
-from app import current_users_gauge
-from app.routes.metrics_route import get_version_number 
+from app import current_users_gauge, get_version
+from app.routes.metrics_route import extract_major_version
 
 
 main_bp = Blueprint('main', __name__)
@@ -9,7 +9,7 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/', methods=['GET'])
 def index():
     # Inc active user number
-    version = get_version_number()
+    version = extract_major_version(get_version())
     current_users_gauge.labels(version=version).inc()
     version_util = VersionUtil()
     lib_version = version_util.get_version()
